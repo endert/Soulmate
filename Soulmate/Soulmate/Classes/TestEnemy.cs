49185aed31c99a2cf01587a7cc9885429e6f0083
Kedreals <ebert.mueden1@freenet.de>
2014-12-18 15:47:07 +0100

    okay jz aber

---
+++ Soulmate/Soulmate/Classes/TestEnemy.cs
@@ -0,0 +1,42 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soulmate.Classes
+{
+    class TestEnemy : AbstractEnemy
+    {
+        Texture enemyTexture = new Texture("Pictures/Player.png");
+
+        public TestEnemy(Vector2f spawnPos, int _lvl)
+        {
+            enemySprite = new Sprite(enemyTexture);
+            enemySprite.Position = spawnPos;
+            lvl = _lvl;
+            hp = 100 + 10 * (lvl - 1);
+            mp = 100 + 10 * (lvl - 1);
+            attackRange = 10f;
+            aggroRange = 560f;
+            movementSpeed = 0.1f;
+        }
+
+        public override void attack()
+        {
+
+        }
+
+        public override void react()
+        {
+           
+        }
+
+        public override void notReact()
+        {
+            
+        }
+    }
+}
