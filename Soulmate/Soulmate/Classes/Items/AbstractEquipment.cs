59bdc5065c8311a7cbb171c7f9606212af235846
Kedreals <ebert.mueden1@freenet.de>
2015-01-20 20:39:44 +0100

    wesrtzuiopü

---
+++ Soulmate/Soulmate/Classes/Items/AbstractEquipment.cs
@@ -0,0 +1,14 @@
+﻿using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soulmate.Classes
+{
+    abstract class AbstractEquipment : AbstractItem
+    {
+        override abstract public void cloneAndDrop(Vector2f dropPosition);
+    }
+}
