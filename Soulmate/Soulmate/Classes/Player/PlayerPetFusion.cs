341da0a42391542a789c47aa43c850dda9e38b4e
Kedreals <ebert.mueden1@freenet.de>
2015-01-20 21:16:47 +0100

    srghhdgh

--- Soulmate/Soulmate/Classes/Player/PlayerPetFusion.cs
+++ Soulmate/Soulmate/Classes/Player/PlayerPetFusion.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,30 @@ using System.Threading.Tasks;
 
 namespace Soulmate.Classes
 {
-    class PlayerPetFusion //: Player
+    class PlayerPetFusion : Player
     {
         Pet fusionedPet;
         Player fusionedPlayer;
+        int 
+
+        Texture[] werwolfTexture = {new Texture("Pictures/Player/PlayerWerwolf/WerwolfFront.png"), new Texture("Pictures/Player/PlayerWerwolf/WerwolfRueck.png"),
+                                   new Texture("Pictures/Player/PlayerWerwolf/WerwolfSeiteRechts.png"), new Texture("Pictures/Player/PlayerWerwolf/WerwolfSeiteLinks.png")};
+
+        public PlayerPetFusion(Pet pet, Player player, int durationInSec)
+        {
+            fusionedPet = pet;
+            fusionedPlayer = player;
+            type = "player";
+            numFacingDirection = player.getNumFacingDirection();
+            facingInDirection = player.getFacingDirection(); // RECHTS
+            sprite = new Sprite(werwolfTexture[numFacingDirection]);
+            sprite.Position = player.getPosition();
+            position = player.getPosition();
+            hitBox = new HitBox(sprite.Position, playerWithoutSwordTexture.Size.X, getHeight());
+            maxHP = player.getMaxHP();
+            currentHP = player.getCurrentHP();
+            att = player.getAtt();
+            def = player.getDef();
+        }
     }
 }
